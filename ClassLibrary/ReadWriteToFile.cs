9907c736810f5793b9812143e33fd4730a813463
DenPas404 / D.Paaske <denpasdk@gmail.com>
2022-07-04 08:28:45 +0200

    Change path for txt file

--- ClassLibrary/ReadWriteToFile.cs
+++ ClassLibrary/ReadWriteToFile.cs
@@ -10,18 +10,22 @@ namespace ClassLibrary
     internal class ReadWriteToFile
     {
         //Path on work pc
-        private const string _path = @"C:\Users\denpaa\source\repos\ThreeCases\ThreeCases\infobase.txt";
+        //private const string _path = @"C:\Users\denpaa\source\repos\ThreeCases\ThreeCases\infobase.txt";
+
+        string fileName = "infobase.txt";
+        string _path;
 
         //Path on home pc
         //private const string _path = @"C:/Users/dpaas/source/repos/ThreeCases/ThreeCases/infobase.txt";
-        
+
         public ReadWriteToFile()
-        { 
+        {
+            _path = Path.Combine(Environment.CurrentDirectory, @"Data\", fileName);
         }
 
         public (string, string) ReadFile()
         {
-            String import = File.ReadAllText(_path);
+            String import = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Data\", fileName));
             var splitted = import.Split(';');
             String controllName = splitted[0].ToLower();
             String controllPass = splitted[1];
@@ -33,8 +37,7 @@ namespace ClassLibrary
 
         public void WriteToFile(String input)
         {
-
-            File.WriteAllText(_path, input);
+            File.WriteAllText(Path.Combine(Environment.CurrentDirectory, @"Data\", fileName, input);
         }
     }
 }
